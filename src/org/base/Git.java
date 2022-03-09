18af68e3dbcfe2a2e78f6a64c108748a9b2b235a
jayakarkancharla <jayakarkancharla4@gmail.com>
2022-03-09 19:35:14 +0530

    dhoni

---
+++ src/org/base/Git.java
@@ -0,0 +1,19 @@
+package org.base;
+
+public class Git {
+private void tc() {
+System.out.println("jay");
+}
+private void tc1() {
+System.out.println("balayya");
+}
+private void tc2() {
+System.out.println("virat");
+}
+public static void main(String[] args) {
+	Git g = new Git();
+	g.tc();
+	g.tc1();
+
+}
+}
